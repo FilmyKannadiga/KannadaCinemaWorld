742d8d49490f4ded91b127d6369d866a8b9f4e73
FilmyKannadiga <naveengowdathegreat@gmail.com>
2026-08-04 18:16:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -93,7 +93,7 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+LEBoz8OBWTc4ZDBl')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+x6tDHJ7nQXA0NGM1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+cjh8HiYXq80wMTI1')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/KCW_Admin_Bot')
 DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/+cjh8HiYXq80wMTI1')
