a2700ff3194e62225b86b27b40748f14420aa117
FilmyKannadiga <naveengowdathegreat@gmail.com>
2026-08-04 14:33:34 +0530

    Update Cinemagoer initialization with access system

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ BTN_URL_REGEX = re.compile(
     r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))"
 )
 
-imdb = Cinemagoer() 
+imdb = Cinemagoer(accessSystem='https://t.me/+x6tDHJ7nQXA0NGM1) 
 TOKENS = {}
 VERIFIED = {}
 BANNED = {}
