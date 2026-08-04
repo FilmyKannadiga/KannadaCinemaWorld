1be9029f80045de821f99d29d2a7fb479493c749
FilmyKannadiga <naveengowdathegreat@gmail.com>
2026-08-04 15:18:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ BTN_URL_REGEX = re.compile(
     r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))"
 )
 
-imdb = Cinemagoer(accessSystem='http')
+imdb = Cinemagoer()
 TOKENS = {}
 VERIFIED = {}
 BANNED = {}
